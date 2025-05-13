f246ac88f877825f5f52d8ca02e1aa1e04196c91
Isa <isabella.smejia@udea.edu.co>
2025-05-12 23:33:43 -0500

    Actualizaciones de algunas funcionalidades del programa

--- src/servicios/TemperaturaCiudadServicio.java
+++ src/servicios/TemperaturaCiudadServicio.java
@@ -4,21 +4,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import javax.swing.JOptionPane;
-
 import entidades.TemperaturaCiudad;
+
 
 public class TemperaturaCiudadServicio {
 
@@ -45,8 +40,6 @@ public class TemperaturaCiudadServicio {
                                 .collect(Collectors.toList());
         }
 
-
-
         public static double getPromedioTemperaturaCiudad(List<TemperaturaCiudad> datos, String ciudad, LocalDate desde,
                         LocalDate hasta) {
                 List<TemperaturaCiudad> datosFiltrados = filtrar(desde, hasta, datos);
@@ -58,26 +51,19 @@ public class TemperaturaCiudadServicio {
                                 .orElse(0.0);
         }
 
-        public static double getMaximo(List<Double> datos) {
-                return datos.isEmpty() ? 0 : datos.stream().mapToDouble(Double::doubleValue).max().orElse(0);
-        }
-
-        public static double getMinimo(List<Double> datos) {
-                return datos.isEmpty() ? 0 : datos.stream().mapToDouble(Double::doubleValue).min().orElse(0);
-        }
-
         public static Map<String, Double> getPromediosTemperaturas(LocalDate desde, LocalDate hasta,
                         List<TemperaturaCiudad> datos) {
-                List<String> ciudades = Arrays.asList("Barranquilla", "Bogotá", "Cali", "Cartagena", "Medellín");
-                Map<String, Double> promedios = new LinkedHashMap<>();
 
-                ciudades.forEach(ciudad -> {
-                        double promedio = getPromedioTemperaturaCiudad(datos, ciudad, desde, hasta);
-                        promedios.put(ciudad, promedio);
-                });
+                List<String> ciudades = datos.stream()
+                                .map(TemperaturaCiudad::getCiudad)
+                                .distinct()
+                                .sorted()
+                                .collect(Collectors.toList());
 
-                return promedios;
-
+                return ciudades.stream()
+                                .collect(Collectors.toMap(
+                                                ciudad -> ciudad,
+                                                ciudad -> getPromedioTemperaturaCiudad(datos, ciudad, desde, hasta)));
         }
 
         public static Map<String, String> getTemperaturasExtremas(LocalDate desde, List<TemperaturaCiudad> datos) {
@@ -85,32 +71,23 @@ public class TemperaturaCiudadServicio {
                 List<TemperaturaCiudad> filtrarDatosFecha = datos.stream()
                                 .filter(dato -> dato.getFecha().equals(desde))
                                 .collect(Collectors.toList());
-                if (filtrarDatosFecha.size() > 0) {
-                        double maxTemperatura = TemperaturaCiudadServicio.getMaximo(
-                                        filtrarDatosFecha.stream().map(TemperaturaCiudad::getTemperatura)
-                                                        .collect(Collectors.toList()));
-                        double minTemperatura = TemperaturaCiudadServicio.getMinimo(
-                                        filtrarDatosFecha.stream().map(TemperaturaCiudad::getTemperatura)
-                                                        .collect(Collectors.toList()));
 
-                        TemperaturaCiudad max = filtrarDatosFecha.stream()
-                                        .filter(dato -> dato.getTemperatura() == maxTemperatura)
-                                        .collect(Collectors.toList()).get(0);
-                        ;
-                        TemperaturaCiudad min = filtrarDatosFecha.stream()
-                                        .filter(dato -> dato.getTemperatura() == minTemperatura)
-                                        .collect(Collectors.toList()).get(0);
-                        String fechaFormateada = desde.toString();
-                        Map<String, String> temperaturasExtremas = new LinkedHashMap<>();
-                        temperaturasExtremas.put("Fecha: ", fechaFormateada);
-                        temperaturasExtremas.put("Ciudad más calurosa:  ",
-                                        max.getCiudad() + " (" + max.getTemperatura() + "°C)\n");
-                        temperaturasExtremas.put("Ciudad más fría:  ",
-                                        min.getCiudad() + " (" + min.getTemperatura() + "°C)");
+                Optional<TemperaturaCiudad> max = filtrarDatosFecha.stream()
+                                .max(Comparator.comparingDouble(TemperaturaCiudad::getTemperatura));
 
-                        return temperaturasExtremas;
-                }
-                JOptionPane.showMessageDialog(null, "No hay datos en esas fechas", "INFORMATION_MESSAGE", JOptionPane.INFORMATION_MESSAGE);
-                return new HashMap<>();
+                Optional<TemperaturaCiudad> min = filtrarDatosFecha.stream()
+                                .min(Comparator.comparingDouble(TemperaturaCiudad::getTemperatura));
+
+                String fechaFormateada = desde.toString();
+
+                Map<String, String> temperaturasExtremas = new LinkedHashMap<>();
+
+                temperaturasExtremas.put("Fecha: ", fechaFormateada);
+                temperaturasExtremas.put("Ciudad más calurosa:  ",
+                                max.get().getCiudad() + " (" + max.get().getTemperatura() + "°C)\n");
+                temperaturasExtremas.put("Ciudad más fría:  ",
+                                min.get().getCiudad() + " (" + min.get().getTemperatura() + "°C)");
+
+                return temperaturasExtremas;
         }
 }
